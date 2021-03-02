52aa4de6cb23ca3a4a4f7c5f300da53574a4bc05
daminort <daminort@gmail.com>
2021-03-02 15:10:03 +0200

    Attempt to use nested CSS (no luck)

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    // require('postcss-nested'),
     require('tailwindcss'),
-    require('postcss-nested'),
+    [
+      "postcss-preset-env",
+      {
+        stage: 3,
+        features: {
+          "nesting-rules": true,
+        },
+      },
+    ],
     require('autoprefixer'),
   ]
 }
